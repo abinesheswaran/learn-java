453401999606ba6e6efb0c2a114f3990747c580f
abinesheswaran <abinesh17a@gmail.com>
2025-02-26 09:42:36 +0530

    add function to Say Hello

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
     public static void main(String args []){
         System.out.println("Hello World!");
+        sayHello("Abinesh");
+    }
+    public static void sayHello(String name){
+        System.out.println("Hello, My name is "+name);
     }
 }
