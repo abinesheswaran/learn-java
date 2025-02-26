c606ce960886b4de8d57c7679370ba99cc20c006
abinesheswaran <abinesh17a@gmail.com>
2025-02-26 10:05:40 +0530

    add basic OOP

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,15 @@
+import OOP_Basics.test1.Bike;
+import OOP_Basics.test1.Car;
+import OOP_Basics.test1.Vehicle;
+
 public class HelloWorld {
     public static void main(String args []){
         System.out.println("Hello World!");
         sayHello("Abinesh");
+        Vehicle car = new Car(1234,"TN","D");
+        System.out.println(car.toString());
+        Bike bike =new Bike(4321,"TN","D");
+        System.out.println(bike.toString());
     }
     public static void sayHello(String name){
         System.out.println("Hello, My name is "+name);
---
+++ OOP_Basics/test1/Bike.java
@@ -0,0 +1,7 @@
+package OOP_Basics.test1;
+
+public class Bike extends Vehicle{
+    public Bike(int id, String state, String district) {
+        super(id, state, district);
+    }
+}
